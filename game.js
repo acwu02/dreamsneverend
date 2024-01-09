8c53d6721e20be5f1fe25c124b98210d24f7807a
Andrew Wu <acwu02@gmail.com>
2024-01-09 14:14:19 -0800

    nevermind

--- game.js
+++ game.js
@@ -1080,7 +1080,7 @@ class Player extends Entity {
         this.baseAtk = PLAYER_ATK_START;
         this.baseDef = PLAYER_DEF_START;
         this.exp = 0;
-        this.gold = 100;
+        this.gold = 0;
         this.def = 1;
 
         this.melatoninFound = 0;
@@ -1526,7 +1526,7 @@ class WhiteRabbit extends Entity {
         super(null, null, "r");
         this.map = map;
         this.life = 0;
-        this.lifespan = 150;
+        this.lifespan = 15;
     }
     move() {
         if (getRandomNumber(1, 2) === 1) {
